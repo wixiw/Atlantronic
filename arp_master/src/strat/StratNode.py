38f516008a5aedff47d1bab4f9daa2afc3be287f
moulineau <moulineau@c9b90f90-cbc1-11df-9d65-af01d29d43da>
2011-05-08 11:20:49 +0000

    modifié le mécanisme des input pour avoir un truc générique
    git-svn-id: svn://team-ard.com/ARP/trunk@424 c9b90f90-cbc1-11df-9d65-af01d29d43da

--- arp_master/src/strat/StratNode.py
+++ arp_master/src/strat/StratNode.py
@@ -2,27 +2,39 @@
 import roslib; roslib.load_manifest('arp_master')
 import rospy
 from arp_core.msg import Obstacle
-
-# variables
-ncallback=0
-obstacle=0
-    
+from arp_core.msg import StartColor
+from arp_core.msg import Start
     
 ###########################  TEMPORAL BEHAVIOR
 
 def StratNode():
+    global inputList
+    
     rospy.init_node('StratNode')
     init()
     rate =rospy.Rate(1)
+    
     while not rospy.is_shutdown():
+        for input in inputList:
+            input.update()
         mainloop()
         rate.sleep()
         
 
 ############################# INITIALISATION
-def init():    
-    rospy.Subscriber("obstacle", Obstacle, obstacleCB)
+def init():
     
+    #creation and linking of inputs
+    global inputList,obstacle,color,start    
+    inputList=[]
+    obstacle= Input("obstacle", Obstacle)
+    color=Input("color", StartColor)
+    start=Input("start", Start)
+    #x = Input()
+    #y = Input()
+    #theta = Input()
+
+    #welcome message
     rospy.loginfo("******************************************************")
     rospy.loginfo("Welcome to Advanced Robotics Platform. I am StratNode.")
     rospy.loginfo("Choose color with button")
@@ -35,20 +47,24 @@ def init():
     
 ############################# MAIN LOOP
 def mainloop():
-    global ncallback,obstacle
-    rospy.loginfo("obstacle=%.3f"%obstacle)
-    rospy.loginfo("callback appelee %i fois"%ncallback)
-    ncallback=0
+    global obstacle
+    rospy.loginfo("obstacle: %i"%obstacle.data.detected)
 
-
-
-########################## CALL BACKS    
-def obstacleCB(data):
-    global ncallback,obstacle
-    obstacle=data.detected
-    ncallback+=1
+########################### INPUT CLASS
+# this is used to have a buffer on inputs, so that they are not changing during the mainloop
+class Input():
+    def __init__(self,topicName,msgType):
+        global inputList
+        self.data=msgType()
+        self.data_lastvalue=msgType()
+        inputList.append(self)
+        rospy.Subscriber(topicName,msgType,self.updateCallBack)
+    def update(self):
+        self.data=self.data_lastvalue
+    def updateCallBack(self, data):
+        self.data_lastvalue=data
     
-
+   
 ########################## EXECUTABLE 
 #shall be always at the end ! so that every function is defined before
 # main function, called by ros
