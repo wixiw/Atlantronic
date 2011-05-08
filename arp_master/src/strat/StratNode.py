e2dec05d5ad02d39b8825e7634333dc8f2ef5e11
moulineau <moulineau@c9b90f90-cbc1-11df-9d65-af01d29d43da>
2011-05-08 09:06:07 +0000

    le stratnode ecoute maintenant l'obstacle d'ailleurs la callback est appelée trop souvent à mon gout
    git-svn-id: svn://team-ard.com/ARP/trunk@419 c9b90f90-cbc1-11df-9d65-af01d29d43da

--- arp_master/src/strat/StratNode.py
+++ arp_master/src/strat/StratNode.py
@@ -1,15 +1,57 @@
 #!/usr/bin/env python
 import roslib; roslib.load_manifest('arp_master')
 import rospy
+from arp_core.msg import Obstacle
+
+# variables
+ncallback=0
+obstacle=0
+    
+    
+###########################  TEMPORAL BEHAVIOR
 
 def StratNode():
-    #pub = rospy.Publisher('chatter', String)
     rospy.init_node('StratNode')
+    init()
+    rate =rospy.Rate(1)
     while not rospy.is_shutdown():
-        str = "Bonjour, je suis le stratnode %s"%rospy.get_time()
-        rospy.loginfo(str)
-        rospy.sleep(5.0)
+        mainloop()
+        rate.sleep()
+        
 
+############################# INITIALISATION
+def init():    
+    rospy.Subscriber("obstacle", Obstacle, obstacleCB)
+    
+    rospy.loginfo("******************************************************")
+    rospy.loginfo("Welcome to Advanced Robotics Platform. I am StratNode.")
+    rospy.loginfo("Choose color with button")
+    rospy.loginfo("Then plug start")
+    rospy.loginfo("Wait for initialisation sequence")
+    rospy.loginfo("And unplug start")
+    rospy.loginfo("******************************************************")
+
+    
+    
+############################# MAIN LOOP
+def mainloop():
+    global ncallback,obstacle
+    rospy.loginfo("obstacle=%.3f"%obstacle)
+    rospy.loginfo("callback appelee %i fois"%ncallback)
+    ncallback=0
+
+
+
+########################## CALL BACKS    
+def obstacleCB(data):
+    global ncallback,obstacle
+    obstacle=data.detected
+    ncallback+=1
+    
+
+########################## EXECUTABLE 
+#shall be always at the end ! so that every function is defined before
+# main function, called by ros
 if __name__ == '__main__':
     try:
         StratNode()
