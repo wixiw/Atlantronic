d033705767a8c255e5ce426ed9f1fae409204f4e
willy <willy@c9b90f90-cbc1-11df-9d65-af01d29d43da>
2011-05-10 14:16:50 +0000

    Séparation des codes des noeuds pour StartA (C++) et SillyWalk (Python)
    git-svn-id: svn://team-ard.com/ARP/trunk@444 c9b90f90-cbc1-11df-9d65-af01d29d43da

--- nobuild_rmo.sh
+++ nobuild_rmo.sh
@@ -4,4 +4,4 @@ touch socket_can/ROS_NOBUILD
 touch rtt_ros_integration_actionlib_msgs/ROS_NOBUILD
 touch rtt_ros_integration_arp_core_msgs/ROS_NOBUILD
 #touch arp_core/ROS_NOBUILD
-#touch arp_hml/ROS_NOBUILD
+touch arp_hml/ROS_NOBUILD
