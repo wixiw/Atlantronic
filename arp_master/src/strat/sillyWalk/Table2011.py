c2e369dd1737d6421ba06c84ae41004e2a59aaf3
moulineau <moulineau@c9b90f90-cbc1-11df-9d65-af01d29d43da>
2011-05-18 22:13:06 +0000

    debut de fonctions de damier
    git-svn-id: svn://team-ard.com/ARP/trunk@488 c9b90f90-cbc1-11df-9d65-af01d29d43da

--- arp_master/src/strat/sillyWalk/Table2011.py
+++ arp_master/src/strat/sillyWalk/Table2011.py
@@ -17,7 +17,11 @@ def getDirection(anglerobot):
     if -3*pi/4<anglerobot<=-pi/4:
         return (0,-1)
 
-    
+#give him a position, it will return the case on which you are
+def getCase(x,y):
+    return Case(1+2*floor(x/350),1+2*floor(y/350))
+
+#represent the robot dimensions    
 class Robot:
     xPion=0.142 
     yPion=-0.087
@@ -37,12 +41,55 @@ class Case:
         y=self.yCenter-(Robot.xPion*sin(theta)+Robot.yPion*cos(theta))
         return (x,y) 
     
-    def returnColor(self):
+    def color(self):
         if (self.i/2+self.j/2)%2==1:
             return 'red'
         else:
             return 'blue'
-        
+    
+    def getClosestInDirection(dir,color):
+        # dir could be (1,1)(-1,0)... etc, meaning "-1 in X and 0 in y"
+        i_test=self.i
+        j_test=self.j
+        inc_i=dir(0)*2
+        inc_j=dir(1)*2
+        while(True):
+            i_test+=inc_i
+            j_test+=inc_j
+            case=Case(i_test,j_test)
+            if case.inTable==False:
+                break
+            if color=='any':
+                return case
+            if case.color()==color:
+                return case
+         
+    def getFurthestInDirection(dir,color):
+        i_test=self.i
+        j_test=self.j
+        inc_i=dir(0)*2
+        inc_j=dir(1)*2
+        last_good_case=None
+        while(True):
+            i_test+=inc_i
+            j_test+=inc_j
+            case=Case(i_test,j_test)
+            if case.inTable==False:
+                break
+            if color=='any':
+                last_good_case= case
+            if case.color()==color:
+                last_good_case= case
+                
+        return last_good_case   
+                
+
+    #will tell if the considered case in within the table            
+    def inTable(self):
+        if self.i>5 or self.j>5 or self.i<-5 or self.j<-5:
+            return False
+        else:
+            return True
 
 class AmbiCaseRed(Case):
     def __init__(self,i_hor,j_vert,color):
