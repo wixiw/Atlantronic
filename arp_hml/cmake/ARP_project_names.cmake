08c2886c05d276449a25020efad2022129f00f4d
willy <willy@c9b90f90-cbc1-11df-9d65-af01d29d43da>
2011-05-19 15:35:10 +0000

    Evolution de la version
    git-svn-id: svn://team-ard.com/ARP/trunk@493 c9b90f90-cbc1-11df-9d65-af01d29d43da

--- arp_hml/cmake/ARP_project_names.cmake
+++ arp_hml/cmake/ARP_project_names.cmake
@@ -13,7 +13,7 @@ set(project_product_name "Advanced Robotics Platform arp_hml Library")
 set(project_file_description "ARP-arp_hml library")
 set(project_comments "develepment in progress")
 set(project_version_major 11)
-set(project_version_minor 0)
-set(project_version_patch 1)
+set(project_version_minor 1)
+set(project_version_patch 0)
 
 ####################################################################################################################
