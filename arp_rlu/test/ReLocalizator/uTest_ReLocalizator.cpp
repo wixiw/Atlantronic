d54be03da70ff8a32be54a01a4caf636fcfe4106
boris <boris@c9b90f90-cbc1-11df-9d65-af01d29d43da>
2011-05-30 11:44:15 +0000

    Refab de code - ReLocalizator est une lib - déport dans un dossier spécifique - uniTest près pour ReLocalizator
    git-svn-id: svn://team-ard.com/ARP/trunk@548 c9b90f90-cbc1-11df-9d65-af01d29d43da

---
+++ arp_rlu/test/ReLocalizator/uTest_ReLocalizator.cpp
@@ -0,0 +1,14 @@
+/*
+ * uTest_CornerDetector.cpp
+ *
+ *  Created on: 28 mai 2011
+ *      Author: Boris
+ */
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE uTest_ReLocalizator
+#include <boost/test/included/unit_test.hpp>
+#include <boost/bind.hpp>
+
+#include "uTest_TableCorner.hpp"
+
---
+++ arp_rlu/test/ReLocalizator/uTest_TableCorner.hpp
@@ -0,0 +1,20 @@
+/*
+ * uTest_ComputeSegment.cpp
+ *
+ *  Created on: 12 sept. 2010
+ *      Author: boris
+ */
+
+
+#include "ReLocalizator/ReLocalizator.hpp"
+#include <math/math.hpp>
+
+using namespace arp_math;
+using namespace Eigen;
+using namespace arp_rlu;
+using namespace std;
+
+BOOST_AUTO_TEST_CASE( TableCorner_Test )
+{
+
+}
