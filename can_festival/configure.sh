f648938ef9ebf2f1f8b7ec1c5a7e63ffbcf7a227
willy <willy@c9b90f90-cbc1-11df-9d65-af01d29d43da>
2011-10-17 08:14:23 +0000

    Intégration Orocos 2.5
    git-svn-id: svn://team-ard.com/ARP/trunk@703 c9b90f90-cbc1-11df-9d65-af01d29d43da

--- can_festival/configure.sh
+++ can_festival/configure.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+if [ $OROCOS_TARGET == "xenomai" ]
+then
+	TIMERS="xeno"
+fi
+if [ $OROCOS_TARGET == "gnulinux" ]
+then
+	TIMERS="unix"
+fi
+
 if [ $# == 1 ]
 then
 	if [ $1 == "debug" ]
 	then
 	cd src
-	./configure --timers=xeno --can=socket --enable-lss --enable-lss-fs --prefix=`rospack find can_festival` --debug=WAR
+	./configure --timers=$TIMERS --can=socket --enable-lss --enable-lss-fs --prefix=`rospack find can_festival` --debug=WAR
 	fi
 else
 	cd src
-	./configure --timers=xeno --can=socket --enable-lss --enable-lss-fs --prefix=`rospack find can_festival` --debug="ERR"
+	./configure --timers=$TIMERS --can=socket --enable-lss --enable-lss-fs --prefix=`rospack find can_festival` --debug="ERR"
 fi
