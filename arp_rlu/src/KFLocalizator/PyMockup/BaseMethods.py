* affichage d'ellipses pour représenter les incertitudes * bug fix : confusion écarts type et variance dans la covariance
git-svn-id: svn://team-ard.com/ARP/trunk@714 c9b90f90-cbc1-11df-9d65-af01d29d43da

arp_rlu/src/KFLocalizator/PyMockup/BaseMethods.py
@@ -45,6 +45,25 @@ def pca(x):
   values, vectors = np.linalg.eig( cov )
   return means, np.sqrt(values), vectors
 
-
+def getEllipseParametersFromEstimate(estim):
+  log = logging.getLogger('getEllipseParametersFromEstimate')
+  cov = estim.covariance[0:2, 0:2]
+  log.debug("cov: \n%s", repr(cov))
+  values, vectors = np.linalg.eig( cov )
+  log.debug("values: \n%s", repr(values))
+  log.debug("vectors: \n%s", repr(vectors))
+  stddev = np.sqrt(values)
+  log.debug("stddev: \n%s", repr(stddev))
+  axe = vectors[0:, np.argmax(stddev)]
+  log.debug("axe: \n%s", repr(axe))
+  height = 3 * np.min(stddev)
+  log.debug("height: %f", height)
+  width = 3 * np.max(stddev)
+  log.debug("width: %f", width)
+  xy = (estim.xRobot, estim.yRobot)
+  log.debug("xy: (%f,%f)", xy[0], xy[1])
+  angle = math.atan2( axe[1], axe[0]) * 180. / np.pi
+  log.debug("angle: %f", angle)
+  return xy, width, height, angle
   
 

arp_rlu/src/KFLocalizator/PyMockup/KFLocalizator.py
@@ -38,9 +38,9 @@ class KFLocalizator:
     self.X = np.array([[initialXPosition], 
                     [initialYPosition], 
                     [initialHeading]])
-    self.P = np.diag((sigmaInitialPosition,
-                   sigmaInitialPosition,
-                   sigmaInitialHeading))
+    self.P = np.diag((sigmaInitialPosition**2,
+                   sigmaInitialPosition**2,
+                   sigmaInitialHeading**2))
     estim = Estimate()
     estim.xRobot = self.X[0,0]
     estim.yRobot = self.X[1,0]
@@ -52,9 +52,9 @@ class KFLocalizator:
     self.buffer = RingBuffer(N)
     self.buffer.append([currentTime, estim])
     
-    self.Q = np.diag((sigmaTransOdoVelocity,
-                   sigmaTransOdoVelocity,
-                   sigmaRotOdoVelocity))
+    self.Q = np.diag((sigmaTransOdoVelocity**2,
+                   sigmaTransOdoVelocity**2,
+                   sigmaRotOdoVelocity**2))
     
     self.sigmaLaserRange = sigmaLaserRange
     self.sigmaLaserAngle = sigmaLaserAngle
@@ -199,8 +199,8 @@ class KFLocalizator:
             IM[0,0] = np.sqrt((self.X[0,0] - xBeacon)**2 + (self.X[1,0] - yBeacon)**2 )
             IM[1,0] = betweenMinusPiAndPlusPi(math.atan2(yBeacon - self.X[1,0], xBeacon - self.X[0,0]) -  self.X[2,0])
             
-            R = np.diag((self.sigmaLaserRange, 
-                         self.sigmaLaserAngle))
+            R = np.diag((self.sigmaLaserRange**2, 
+                         self.sigmaLaserAngle**2))
             
             
             log.debug("---")
@@ -236,9 +236,9 @@ class KFLocalizator:
             # print "KFLocalizator - newScan() : IM="; print IM
             
             
-            R = np.diag((self.sigmaLaserRange, 
-                         self.sigmaLaserAngle,
-                         self.sigmaSegmentHeading))
+            R = np.diag((self.sigmaLaserRange**2, 
+                         self.sigmaLaserAngle**2,
+                         self.sigmaSegmentHeading**2))
             
             log.debug("---")
             log.debug("mesure : Y.r= %f  Y.theta= %f  Y.beta= %f", r, theta, heading)

arp_rlu/test/KFLocalizator/PyMockup/tests/test_KFLocalizator_Static.py
@@ -3,12 +3,14 @@ import sys
 sys.path.append( "../../../../src/KFLocalizator/PyMockup" )
 
 import logging
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 log = logging.getLogger('main')
 
 import numpy as np
 import random
 import matplotlib.pyplot as plt
+from matplotlib.collections import PatchCollection
+import matplotlib.patches as mpatches
 
 from KFLocalizator import *
 from LRFSimulator import *
@@ -41,10 +43,8 @@ graine_ = random.randint(0,1000)
 #graine = 394 #graine_ = 830
 
 
-
-graine = 461
-graine_ = 441
-
+#graine = 66 
+#graine_ = 562
 
 random.seed(graine)
 log.info("graine pour la position réelle :%d", graine)
@@ -177,6 +177,15 @@ xArrowEnd = 0.1 * np.cos(initialHeading)
 yArrowEnd = 0.1 * np.sin(initialHeading)
 arrow = plt.Arrow(xArrowBeg, yArrowBeg, xArrowEnd, yArrowEnd, width=0.01, color="red")
 ax.add_patch(arrow)
+if params.visu_cfg["ellipse"]:
+  estim = Estimate()
+  estim.xRobot = kfloc.X[0,0]
+  estim.yRobot = kfloc.X[1,0]
+  estim.hRobot = kfloc.X[2,0]
+  estim.covariance = kfloc.P
+  xy, width, height, angle = getEllipseParametersFromEstimate(estim)
+  ellipse = mpatches.Ellipse(xy, width, height, angle, alpha=0.3, ec="red", fc = "none")
+  ax.add_patch(ellipse)
 
 xArrowBeg = trueX
 yArrowBeg = trueY
@@ -225,8 +234,7 @@ yOld = initialYPosition
 
 time = 0.01
 
-Ntour = 8
-for k in range(Ntour):
+for k in range(params.simu_cfg["Nscans"]):
     log.info("==============================================")
     log.info("==============================================")
     log.info(" TOUR %d", k)
@@ -236,9 +244,9 @@ for k in range(Ntour):
     odoDurationInSec = 0.1
     ov = OdoVelocity()
     for t in np.arange(time, time + odoDurationInSec, 0.01):
-      ov.vx = random.normalvariate(0., params.kf_cfg["sigmaTransOdoVelocity"])
-      ov.vy = random.normalvariate(0., params.kf_cfg["sigmaTransOdoVelocity"])
-      ov.vh = random.normalvariate(0., params.kf_cfg["sigmaRotOdoVelocity"])
+      ov.vx = random.normalvariate(0., params.simu_cfg["sigmaTransOdoVelocity"])
+      ov.vy = random.normalvariate(0., params.simu_cfg["sigmaTransOdoVelocity"])
+      ov.vh = random.normalvariate(0., params.simu_cfg["sigmaRotOdoVelocity"])
       kfloc.newOdoVelocity(t, ov)
     time = time + odoDurationInSec
     
@@ -292,16 +300,23 @@ for k in range(Ntour):
       log.debug( "    sur x (en mm): %f", (estim2[1].xRobot - trueX) * 1000.)
       log.debug( "    sur y (en mm): %f", (estim2[1].yRobot - trueY) * 1000.)
       log.debug( "    en cap (deg) : %f", betweenMinusPiAndPlusPi( estim2[1].hRobot - trueH ) *180./np.pi)
-      log.debug("covariance :\n%s", repr(estim2[1].covariance))    
-      xArrowBeg = estim2[1].xRobot
-      yArrowBeg = estim2[1].yRobot
-      xArrowEnd = 0.1 * np.cos(estim2[1].hRobot)
-      yArrowEnd = 0.1 * np.sin(estim2[1].hRobot)
-      arrow = plt.Arrow(xArrowBeg, yArrowBeg, xArrowEnd, yArrowEnd, width=0.005, alpha=0.3, color="green")
-      ax.add_patch(arrow)
-      ax.plot( [xOld, xArrowBeg], [yOld, yArrowBeg], '-b' )
-      xOld = xArrowBeg
-      yOld = yArrowBeg
+      log.debug("covariance :\n%s", repr(estim2[1].covariance))
+
+      if params.visu_cfg["intermediary_arrow"]:    
+        xArrowBeg = estim2[1].xRobot
+        yArrowBeg = estim2[1].yRobot
+        xArrowEnd = 0.1 * np.cos(estim2[1].hRobot)
+        yArrowEnd = 0.1 * np.sin(estim2[1].hRobot)
+        arrow = plt.Arrow(xArrowBeg, yArrowBeg, xArrowEnd, yArrowEnd, width=0.005, alpha=0.3, color="green")
+        ax.add_patch(arrow)
+        ax.plot( [xOld, xArrowBeg], [yOld, yArrowBeg], '-b' )
+        xOld = xArrowBeg
+        yOld = yArrowBeg
+      
+      if params.visu_cfg["intermediary_ellipse"]:
+        xy, width, height, angle = getEllipseParametersFromEstimate(estim2[1])
+        ellipse = mpatches.Ellipse(xy, width, height, angle, alpha=0.3, ec="green", fc="none")
+        ax.add_patch(ellipse)
       
       
       
@@ -326,8 +341,13 @@ for k in range(Ntour):
     yArrowBeg = estim2[1].yRobot
     xArrowEnd = 0.1 * np.cos(estim2[1].hRobot)
     yArrowEnd = 0.1 * np.sin(estim2[1].hRobot)
-    arrow = plt.Arrow(xArrowBeg, yArrowBeg, xArrowEnd, yArrowEnd, width=0.01, alpha=0.3)
+    arrow = plt.Arrow(xArrowBeg, yArrowBeg, xArrowEnd, yArrowEnd, width=0.01, alpha=0.3, color="blue")
     ax.add_patch(arrow)
+    
+    if params.visu_cfg["ellipse"]:
+      xy, width, height, angle = getEllipseParametersFromEstimate(estim2[1])
+      ellipse = mpatches.Ellipse(xy, width, height, angle, alpha=0.1, color="blue")
+      ax.add_patch(ellipse)
     
     ax.plot( [xOld, xArrowBeg], [yOld, yArrowBeg], '-b' )
     xOld = xArrowBeg
@@ -352,5 +372,7 @@ for k in range(Ntour):
 log.info("graine pour la position réelle :%d", graine)
 log.info("graine pour la simulation :%d", graine_)
 
-#ax.axis([trueX-0.2, trueX+0.2, trueY-0.15, trueY+0.15])
+if params.visu_cfg["zoom"]:
+  ax.axis([trueX-0.2, trueX+0.2, trueY-0.15, trueY+0.15])
+  
 plt.show()

arp_rlu/test/KFLocalizator/PyMockup/unittests/unittest_BaseMethods.py
@@ -67,7 +67,18 @@ class TestBaseMethods(unittest.TestCase):
     self.assertTrue(np.array_equiv( y[3], np.array([[1., 2., 3.], [4., 5., 6.]]) ))
     self.assertTrue(np.array_equiv( y[4], np.array([[2., 4., 6.], [8., 10., 12.]]) ))
 
-
+  def testGetEllipseParametersFromEstimate1(self):
+    estim = Estimate()
+    estim.xRobot = 10.
+    estim.yRobot = -3.
+    estim.covariance = np.diag((3., 2., 0.))
+    xy, width, height, angle = getEllipseParametersFromEstimate(estim)
+    self.assertAlmostEqual( xy[0], 10 )
+    self.assertAlmostEqual( xy[1], -3 )
+    self.assertAlmostEqual( width, 3. * math.sqrt(3.) )
+    self.assertAlmostEqual( height, 3. * math.sqrt(2.) )
+    self.assertAlmostEqual( angle, 0. )
+    
     
 if __name__ == '__main__':
     logging.basicConfig(level=logging.ERROR) 