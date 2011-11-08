362e89eff0976622bb833bd2432e4a2c08abb520
willy <willy@c9b90f90-cbc1-11df-9d65-af01d29d43da>
2011-11-08 12:29:36 +0000

    Corrections diverses pour tenter de builder orocos/xenomai de façon transparente
    git-svn-id: svn://team-ard.com/ARP/trunk@731 c9b90f90-cbc1-11df-9d65-af01d29d43da

--- arp_hml/script/orocos/deployment/deploy_ubiquity.lua
+++ arp_hml/script/orocos/deployment/deploy_ubiquity.lua
@@ -13,21 +13,26 @@ dofile("script/orocos/deployment/ubiquity/motor_deployer.lua");
 dofile("script/orocos/deployment/ubiquity/io_deployer.lua");
 dofile("script/orocos/deployment/ubiquity/hml_itf_deployer.lua");
 
+print("... load components")
+
 CanDeployer:load()
 MotorDeployer:load()
 IoDeployer:load()
 HmlItfDeployer:load()
 
-
+print("... connect components")
 CanDeployer:connect()
 MotorDeployer:connect()
 IoDeployer:connect()
+print("... BITE 3")
 HmlItfDeployer:connect()
+print("... BITE 4")
 
-CanDeployer:start()
-MotorDeployer:start()
-IoDeployer:start()
-HmlItfDeployer:start()
+print("... start components")
+--CanDeployer:start()
+--MotorDeployer:start()
+--IoDeployer:start()
+--HmlItfDeployer:start()
 
 print("fin déploiment arp_hml")
 print("====================")
