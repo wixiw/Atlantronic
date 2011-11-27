384fa28bb0d4ac1119edca31a015f508502e023d
boris <boris@c9b90f90-cbc1-11df-9d65-af01d29d43da>
2011-11-27 22:23:07 +0000

    * conversion TwistWithCovarianceStamped en OdoVelocity * branchement du prédicteur * publication de la pose
    git-svn-id: svn://team-ard.com/ARP/trunk@743 c9b90f90-cbc1-11df-9d65-af01d29d43da

--- arp_rlu/src/KFLocalizator/PyROSMockup/KFLocalizatorNode.py
+++ arp_rlu/src/KFLocalizator/PyROSMockup/KFLocalizatorNode.py
@@ -4,12 +4,17 @@ import roslib; roslib.load_manifest('arp_rlu')
 import rospy
 import sys
 sys.path.append( "./src/KFLocalizator/PyMockup" )
+
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import TwistWithCovarianceStamped
+from geometry_msgs.msg import PoseWithCovarianceStamped
 from std_srvs.srv import *
 from arp_rlu.srv import *
+
+import math
 import KFLocalizator
 import params_KFLocalizator_Node as params
+from BaseClasses import OdoVelocity
 
 class KFLocalizatorNode():
     
@@ -17,17 +22,19 @@ class KFLocalizatorNode():
     rospy.init_node('KFLocalizator', anonymous=True)
     rospy.Subscriber("/scan", LaserScan, self.update)
     rospy.Subscriber("/odovelocity", TwistWithCovarianceStamped, self.predict)
+    self.pub = rospy.Publisher("/KFLocalizator/pose", PoseWithCovarianceStamped)
     rospy.Service('start', Empty, self.start)
     rospy.Service('stop', Empty, self.stop)
     rospy.Service('initialize', KFLocInit, self.cb_Init)
       
     self.kfloc = KFLocalizator.KFLocalizator()
     self.run = False
+    self.initialize(0., 0., 0.)
     rospy.loginfo(rospy.get_name() + " is running")
     rospy.spin()
     
   def initialize(self, initialXPosition, initialYPosition, initialHeading):
-    self.kfloc.initialize(0.,100,
+    self.kfloc.initialize(rospy.get_time(),100,
                           initialXPosition, 
                           initialYPosition, 
                           initialHeading, 
@@ -42,10 +49,18 @@ class KFLocalizatorNode():
   def predict(self, data):
     if not self.run:
       return
-    
-    rospy.loginfo(rospy.get_name() + " predict!")
-    # convert TwistWithCovarianceStamped into OdoVelocity
-    # self.kfloc.newOdoVelocity(...)
+    rospy.loginfo(rospy.get_name() + " predicting...")
+    currentT = data.header.stamp.to_sec()
+    ov = OdoVelocity()
+    ov.vx = data.twist.twist.linear.x
+    ov.vy = data.twist.twist.linear.y
+    ov.vh = data.twist.twist.angular.z
+    sigmaXOdo_ = data.twist.covariance[ 0]
+    sigmaYOdo_ = data.twist.covariance[ 7]
+    sigmaHOdo_ = data.twist.covariance[35]
+    self.kfloc.newOdoVelocity(currentT, ov, sigmaXOdo_, sigmaYOdo_, sigmaHOdo_)
+    self.publishPose()
+    rospy.loginfo(rospy.get_name() + " predict OK")
       
   def update(self, data):
     if not self.run:
@@ -69,7 +84,25 @@ class KFLocalizatorNode():
     self.initialize(req.x, req.y, req.h)
     rospy.loginfo(rospy.get_name() + " initialized with x=" + str(req.x) + " y=" + str(req.y) + " h=" + str(req.h))
     return KFLocInitResponse(True)
-
+  
+  def publishPose(self):
+    estim = self.kfloc.getBestEstimate()
+    p = PoseWithCovarianceStamped()
+    p.header.stamp = rospy.Time.from_sec(estim[0])
+    p.pose.pose.position.x = estim[1].xRobot
+    p.pose.pose.position.y = estim[1].yRobot
+    p.pose.pose.orientation.z = math.sin(estim[1].hRobot/2.)
+    p.pose.pose.orientation.w = math.cos(estim[1].hRobot/2.)
+    p.pose.covariance[ 0] = estim[1].covariance[0,0]
+    p.pose.covariance[ 1] = estim[1].covariance[1,0]
+    p.pose.covariance[ 5] = estim[1].covariance[2,0]
+    p.pose.covariance[ 6] = estim[1].covariance[0,1]
+    p.pose.covariance[ 7] = estim[1].covariance[1,1]
+    p.pose.covariance[11] = estim[1].covariance[2,1]
+    p.pose.covariance[30] = estim[1].covariance[0,2]
+    p.pose.covariance[31] = estim[1].covariance[1,2]
+    p.pose.covariance[35] = estim[1].covariance[2,2]
+    self.pub.publish(p)
 
 if __name__ == '__main__':
   try:
