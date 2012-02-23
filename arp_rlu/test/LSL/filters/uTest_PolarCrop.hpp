3045e4e6bf16bb396eb03a61226d88b782bb69ba
boris <boris@c9b90f90-cbc1-11df-9d65-af01d29d43da>
2012-02-23 21:25:44 +0000

    Implémentation du PolarCrop (complète et unitestée)
    git-svn-id: svn://team-ard.com/ARP/trunk@858 c9b90f90-cbc1-11df-9d65-af01d29d43da

--- arp_rlu/test/LSL/filters/uTest_PolarCrop.hpp
+++ arp_rlu/test/LSL/filters/uTest_PolarCrop.hpp
@@ -82,6 +82,60 @@ BOOST_AUTO_TEST_CASE( Test_DefaultParams )
     }
 }
 
+BOOST_AUTO_TEST_CASE( Test_minRangeNul )
+{
+    lsl::LaserScan rawScan;
+    MatrixXd d(3, 5);
+    d.row(0) <<  1.0,  1.01, 1.02, 1.03, 1.04;
+    d.row(1) <<  1.3,  0.05, 3.2, 11.5,  2.01;
+    d.row(2) << -2.65, -1.3, 0.1,  0.7,  2.7;
+
+    rawScan.setPolarData(d);
+
+    lsl::PolarCrop::Params p;
+    p.minRange = VectorXd::Zero(0);
+    p.maxRange = 10.0 * VectorXd::Ones(1);
+    p.minTheta = -PI;
+    p.maxTheta = PI;
+    lsl::LaserScan filtScan = lsl::PolarCrop::apply(rawScan, p);
+
+    MatrixXd filt = filtScan.getPolarData();
+
+    for(int n = 0 ; n < filt.cols() ; n++)
+    {
+        BOOST_CHECK_EQUAL( filt(0,n), d(0,n) );
+        BOOST_CHECK_EQUAL( filt(1,n), d(1,n) );
+        BOOST_CHECK_EQUAL( filt(2,n), d(2,n) );
+    }
+}
+
+BOOST_AUTO_TEST_CASE( Test_maxRangeNul )
+{
+    lsl::LaserScan rawScan;
+    MatrixXd d(3, 5);
+    d.row(0) <<  1.0,  1.01, 1.02, 1.03, 1.04;
+    d.row(1) <<  1.3,  0.05, 3.2, 11.5,  2.01;
+    d.row(2) << -2.65, -1.3, 0.1,  0.7,  2.7;
+
+    rawScan.setPolarData(d);
+
+    lsl::PolarCrop::Params p;
+    p.minRange = 0.1 * VectorXd::Ones(1);
+    p.maxRange = VectorXd::Zero(0);
+    p.minTheta = -PI;
+    p.maxTheta = PI;
+    lsl::LaserScan filtScan = lsl::PolarCrop::apply(rawScan, p);
+
+    MatrixXd filt = filtScan.getPolarData();
+
+    for(int n = 0 ; n < filt.cols() ; n++)
+    {
+        BOOST_CHECK_EQUAL( filt(0,n), d(0,n) );
+        BOOST_CHECK_EQUAL( filt(1,n), d(1,n) );
+        BOOST_CHECK_EQUAL( filt(2,n), d(2,n) );
+    }
+}
+
 BOOST_AUTO_TEST_CASE( Test_Range_1 )
 {
     lsl::LaserScan rawScan;
@@ -131,8 +185,8 @@ BOOST_AUTO_TEST_CASE( Test_Range_2 )
 
     MatrixXd filt = filtScan.getPolarData();
 
-    VectorXi ifilt = VectorXi(2);
-    ifilt << 0, 4;
+    VectorXi ifilt = VectorXi(3);
+    ifilt << 0, 1, 4;
     BOOST_CHECK_EQUAL( filt.cols(), ifilt.size() );
     for(int n = 0 ; n < ifilt.size() ; n++)
     {
@@ -152,9 +206,9 @@ BOOST_AUTO_TEST_CASE( Test_Range_3 )
 
     rawScan.setPolarData(d);
 
-    VectorXd minr;
+    VectorXd minr(5);
     minr << 0.5, 0.3, 1.1, 1.0, 1.5;
-    VectorXd maxr;
+    VectorXd maxr(5);
     maxr << 2.0, 0.5, 3.1, 12.0, 2.1;
 
     lsl::PolarCrop::Params p;
