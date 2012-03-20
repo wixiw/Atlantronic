22b4fce332724085e217559ff1fb6067f2625d2d
boris <boris@c9b90f90-cbc1-11df-9d65-af01d29d43da>
2012-03-20 20:44:38 +0000

    reactivation de tous les tests unitaires
    git-svn-id: svn://team-ard.com/ARP/trunk@900 c9b90f90-cbc1-11df-9d65-af01d29d43da

--- arp_rlu/test/KFL/uTest_KFL.cpp
+++ arp_rlu/test/KFL/uTest_KFL.cpp
@@ -42,9 +42,9 @@ BOOST_AUTO_TEST_CASE( testdummy )
 }
 BOOST_AUTO_TEST_SUITE_END()
 
-//#include "uTest_BeaconDetector.hpp"
+#include "uTest_BeaconDetector.hpp"
 
 #include "BFL/uTest_BFLWrapper.hpp"
 //
-//#include "uTest_KFLocalizator.hpp"
+#include "uTest_KFLocalizator.hpp"
 
