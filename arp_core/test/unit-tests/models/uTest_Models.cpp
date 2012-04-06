1a7c9654b20e00f352680502282169510f958085
boris <boris@c9b90f90-cbc1-11df-9d65-af01d29d43da>
2012-04-06 17:05:22 +0000

    nettoyage de l'intialisation du Logger dans les unittests
    git-svn-id: svn://team-ard.com/ARP/trunk@986 c9b90f90-cbc1-11df-9d65-af01d29d43da

--- arp_core/test/unit-tests/models/uTest_Models.cpp
+++ arp_core/test/unit-tests/models/uTest_Models.cpp
@@ -17,26 +17,9 @@ using namespace arp_core::log;
 BOOST_AUTO_TEST_SUITE( unittest_dummy )
 BOOST_AUTO_TEST_CASE( testdummy )
 {
-    //  arp_rlu::kfl::Logger::InitNull("uT_KFL", DEBUG);
+    //  arp_model::Logger::InitNull("uT_KFL", DEBUG);
     arp_model::Logger::InitConsole("uT_Models", DEBUG);
-//      arp_rlu::kfl::Logger::InitFile("uT_KFL", DEBUG);
-
-//      arp_rlu::kfl::Log( FATAL )  << "fatal";
-//      arp_rlu::kfl::Log( EMERG )  << "emerg";
-//      arp_rlu::kfl::Log( ALERT )  << "alert";
-//      arp_rlu::kfl::Log( CRIT )   << "crit";
-//      arp_rlu::kfl::Log( ERROR )  << "error";
-//      arp_rlu::kfl::Log( WARN )   << "warn";
-//      arp_rlu::kfl::Log( NOTICE ) << "notice";
-//      arp_rlu::kfl::Log( INFO )   << "info";
-//      arp_rlu::kfl::Log( DEBUG )  << "debug";
-
-    //  boost::unit_test::unit_test_log.set_threshold_level( boost::unit_test::log_successful_tests );
-    //  boost::unit_test::unit_test_log.set_threshold_level( boost::unit_test::log_messages );
-    //  boost::unit_test::unit_test_log.set_threshold_level( boost::unit_test::log_warnings );
-    //  boost::unit_test::unit_test_log.set_threshold_level( boost::unit_test::log_all_errors );
-
-    //  boost::unit_test::unit_test_log.set_stream( std::cout );
+//      arp_model::Logger::InitFile("uT_KFL", DEBUG);
 
 }
 BOOST_AUTO_TEST_SUITE_END()
