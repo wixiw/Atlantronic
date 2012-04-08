5b13db9b7b50b891907fef3d09f2189683b5b5d8
willy <willy@c9b90f90-cbc1-11df-9d65-af01d29d43da>
2012-04-08 01:54:35 +0000

    git-svn-id: svn://team-ard.com/ARP/trunk@1010 c9b90f90-cbc1-11df-9d65-af01d29d43da

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,9 @@
 # this file is executed by jenkins when the ARD job is fired. 
 # This prevent from saving datas in the configuration interface of Jenkins which is neither versionned or backuped
 
+#This scrip is executed in the parent dir of ard
+
 . /opt/env.sh
 
-rosmake arp_master
+cd ard/arp_core
+make -j2
