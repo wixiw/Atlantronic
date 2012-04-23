9b845738b646d2d23f60d19858a4b0c3e0c92a91
willy <willy@c9b90f90-cbc1-11df-9d65-af01d29d43da>
2012-04-23 20:09:01 +0000

    git-svn-id: svn://team-ard.com/ARP/trunk@1132 c9b90f90-cbc1-11df-9d65-af01d29d43da

--- can_festival/configure.sh
+++ can_festival/configure.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-#if [ $OROCOS_TARGET == "xenomai" ]
-"then
-#	TIMERS="xeno"
-#fi
-#if [ $OROCOS_TARGET == "gnulinux" ]
-#then
-#	TIMERS="unix"
-#fi
+if [[ $CAN_FLAVOR == "xenomai" ]]
+then
+	echo "ARD : CanFestival will be compiled against xenomai timers"
+	TIMERS="xeno"
+fi
+if [[ $CAN_FLAVOR == "gnulinux" ]]
+then
+	echo "ARD : CanFestival will be compiled against gnulinux timers"
+	TIMERS="unix"
+fi
 
-TIMERS="xeno"
 
 if [ $# == 1 ]
 then
 	if [ $1 == "debug" ]
 	then
 	cd src
-	./configure --timers=$TIMERS --can=socket --enable-lss --enable-lss-fs --prefix=`rospack find can_festival` --debug=WAR
+	./configure --timers=$TIMERS --can=socket --enable-lss --enable-lss-fs --prefix=`rospack find can_festival` --debug=ERR
 	fi
 else
 	cd src
-	./configure --timers=$TIMERS --can=socket --enable-lss --enable-lss-fs --prefix=`rospack find can_festival` --debug="ERR"
+	./configure --timers=$TIMERS --can=socket --enable-lss --enable-lss-fs --prefix=`rospack find can_festival` --debug=ERR
 fi
