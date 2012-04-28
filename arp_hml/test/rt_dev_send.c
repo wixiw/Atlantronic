31c776ede2247c81a24b36a6b5d7a60515c66600
willy <willy@c9b90f90-cbc1-11df-9d65-af01d29d43da>
2012-04-28 01:10:13 +0000

    Ajout du binding de la socket pour le test unitaire d'envois de message can RT
    git-svn-id: svn://team-ard.com/ARP/trunk@1171 c9b90f90-cbc1-11df-9d65-af01d29d43da

--- arp_hml/test/rt_dev_send.c
+++ arp_hml/test/rt_dev_send.c
@@ -20,11 +20,15 @@ int main()
     struct ifreq ifr;
     int socket;
     int ret;
+    struct sockaddr_can addr;
 
     //open socket
     socket = rt_dev_socket (PF_CAN, SOCK_RAW, CAN_RAW);
     if( socket < 0 )
+    {
         printf("Failed to open socket %d", socket);
+        return 1;
+    }
     else
         printf("Opened socket %d\n",socket);
 
@@ -33,9 +37,22 @@ int main()
     ret = rt_dev_ioctl(socket, SIOCGIFINDEX, &ifr);
     printf("s=%d, ifr_name=%s\n", socket, ifr.ifr_name);
     if (ret < 0)
-        printf(stderr, "rt_dev_ioctl GET_IFINDEX: %s\n", strerror(-ret));
+    {
+        printf("rt_dev_ioctl GET_IFINDEX: %s\n", strerror(-ret));
+        return 1;
+    }
     else
         printf("Ioctl succeed\n");
+
+    addr.can_family = AF_CAN;
+    addr.can_ifindex = ifr.ifr_ifindex;
+    if (rt_dev_bind(socket, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+    {
+        printf("Bind failed \n");
+        return 1;
+    }
+    else
+        printf("Bind succeed\n");
 
     //send message
     ret = rt_dev_send(socket,&frame,sizeof(frame),0);
@@ -48,11 +65,13 @@ int main()
     mlockall(MCL_CURRENT | MCL_FUTURE);
     ret = rt_task_shadow(&rt_task_desc, "MyTask", 0, 0);
     if (ret)
-        printf(stderr, "rt_task_shadow: %s\n", strerror(-ret));
+        printf("rt_task_shadow: %s\n", strerror(-ret));
     else
         printf("Shadowing ok\n");
 
     //send message RT
+    frame.can_id = 0x300;
+    frame.can_dlc = 8;
     ret = rt_dev_send(socket,&frame,sizeof(frame),0);
     if( ret < 0 )
         printf("Failed to send RT %d (%s)\n", ret, strerror (-ret));
