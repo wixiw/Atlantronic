74928cf17628ee1fb6a17f9dbf5e4289a3a890db
willy <willy@c9b90f90-cbc1-11df-9d65-af01d29d43da>
2012-05-11 12:30:38 +0000

    correction des valeurs par défaut de la télémétrie
    git-svn-id: svn://team-ard.com/ARP/trunk@1321 c9b90f90-cbc1-11df-9d65-af01d29d43da

--- arp_master/script/orocos/deployment/lua/telemetry.lua
+++ arp_master/script/orocos/deployment/lua/telemetry.lua
@@ -109,7 +109,7 @@ function Telemetry:report()
 	Telemetry:reportRobotState()
 	--Telemetry:reportMotorState()
 	--Telemetry:reportTiming()
-	Telemetry:reportOmnidirect()
+	--Telemetry:reportOmnidirect()
 
 	
 	
