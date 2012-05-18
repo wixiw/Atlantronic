384f20c44f737a01b0b2b6b3029bf3c1485a7a82
moulineau <moulineau@c9b90f90-cbc1-11df-9d65-af01d29d43da>
2012-05-18 06:34:15 +0000

    suppression des rosparam ...
    git-svn-id: svn://team-ard.com/ARP/trunk@1449 c9b90f90-cbc1-11df-9d65-af01d29d43da

--- arp_master/src/ros/SpeedReducerAntico.py
+++ arp_master/src/ros/SpeedReducerAntico.py
@@ -27,15 +27,19 @@ class SpeedReducerAntico:
         #la vitesse est limitee a reduced_min_speed si l'adversaire est a moins de min_distance de nous
         #sinon la vitesse est limitee en fonction de (distanceOpponent-reduced_min_distance) a condition 
         # d'etre dans le cone +/-search_angle/2
-        #   pour faire une rampe de freinage de valeur decc
-        try:
-            self.min_distance = rospy.get_param('/SpeedReducer/min_distance', Float64)
-            self.reduced_min_speed = rospy.get_param('/SpeedReducer/reduced_min_speed', Float64)
-            self.search_angle = rospy.get_param('/SpeedReducer/search_angle', Float64)
-            self.decc = rospy.get_param('/SpeedReducer/decc', Float64)
-        except KeyError:
-            rospy.logerr("Failed to find cropping_distance rosparams.") 
-            raise RuntimeError
+#        #   pour faire une rampe de freinage de valeur decc
+#        try:
+#            self.min_distance = rospy.get_param('/SpeedReducer/min_distance', Float64)
+#            self.reduced_min_speed = rospy.get_param('/SpeedReducer/reduced_min_speed', Float64)
+#            self.search_angle = rospy.get_param('/SpeedReducer/search_angle', Float64)
+#            self.decc = rospy.get_param('/SpeedReducer/decc', Float64)
+#        except KeyError:
+#            rospy.logerr("Failed to find cropping_distance rosparams.") 
+#            raise RuntimeError
+
+        self.min_distance = 0.500
+        self.reduced_min_speed = 0.000
+        self.decc = 1.0
 
         rospy.wait_for_service('MotionControl/setVMax')            
         self.setVMax_srv=rospy.ServiceProxy("MotionControl/setVMax",SetVMax)
@@ -85,14 +89,14 @@ class SpeedReducerAntico:
                     #rospy.loginfo("SpeedReducer : adversaire dans ma trajectoire")
                     #l'adversaire est tres proche
                     if distance <= self.min_distance:
-                        #rospy.loginfo("SpeedReducer : min %s", self.reduced_min_speed)
+                        rospy.loginfo("SpeedReducer : min %s", self.reduced_min_speed)
                         self.setVmax(self.reduced_min_speed)
                     #on ralentit en fonction de la distance de l'adversaire
                     else:
                         dx = distance - self.min_distance
                         dh = normalizeAngle(opp.closest_angle - self.pose.theta)
                         v = sqrt(2.0*self.decc*dx) + self.reduced_min_speed
-                        #rospy.loginfo("SpeedReducer : prop %s", v)
+                        rospy.loginfo("SpeedReducer : prop %s", v)
                         self.setVmax(v)
                         
                         #if fabs(dh) <= self.search_angle/2.0:
@@ -102,6 +106,7 @@ class SpeedReducerAntico:
                         #    self.unsetVmax()
                             #rospy.loginfo("SpeedReducer : prop out of angle (dh %s = %s - %d)",dh,opp.closest_angle,self.pose.theta)
                 else:
+                    rospy.loginfo("SpeedReducer : unset")
                     self.unsetVmax()
                 
             # fin du test si j'etais en translation
