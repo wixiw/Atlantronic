49f738e40d51d925eb03046f09cb783ada2238bc
willy <willy@c9b90f90-cbc1-11df-9d65-af01d29d43da>
2012-09-26 23:07:36 +0000

    Squelette pour la start 2013
    git-svn-id: svn://team-ard.com/ARP/trunk@1476 c9b90f90-cbc1-11df-9d65-af01d29d43da

---
+++ arp_master/src/arp_master/strat_2013/common_2013/Table2013.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import roslib; roslib.load_manifest('arp_master')
+from arp_master.util.TableVierge import *
+
+import rospy
+import smach
+
+from arp_master.util.Data import *
+
+#il faut voir ca comme un namespace
+class Table2013(TableVierge):
+        #
+        # Points d'interet
+        #     les points sont defini cote rouge comme toujours
+        
+        #bouteilles
+        #P_BOTTLE_CLOSE      = Point(0.860,-0.900)
+
+        
+        # 
+        # Memoire des actions
+        #
+        objectOneEmpty = False
+
+        
+        @staticmethod
+        def printStratInfo():
+            rospy.loginfo("*********************************************")
+            rospy.loginfo("StratInfo :")
+            
+            rospy.loginfo("")
+            rospy.loginfo("*********************************************")
+            
+            
---
+++ arp_master/src/arp_master/strat_2013/common_2013/__init__.py
@@ -0,0 +1,5 @@
+print("Importing common_2012 module")
+
+from Robot2013 import *
+from Table2013 import *
+
