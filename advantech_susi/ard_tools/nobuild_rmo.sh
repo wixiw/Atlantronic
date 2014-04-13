5eafd4be85d38c3b53a8ad4974c6a8861addbe50
willy <willy@c9b90f90-cbc1-11df-9d65-af01d29d43da>
2014-04-13 00:21:21 +0000

    delete unused files
    git-svn-id: svn://team-ard.com/ARP/trunk@1780 c9b90f90-cbc1-11df-9d65-af01d29d43da

--- advantech_susi/ard_tools/nobuild_rmo.sh
+++
@@ -1,7 +0,0 @@
-touch advantech_susi/ROS_NOBUILD
-touch can_festival/ROS_NOBUILD
-touch socket_can/ROS_NOBUILD
-touch rtt_ros_integration_actionlib_msgs/ROS_NOBUILD
-touch rtt_ros_integration_arp_core_msgs/ROS_NOBUILD
-#touch arp_core/ROS_NOBUILD
-touch arp_hml/ROS_NOBUILD
