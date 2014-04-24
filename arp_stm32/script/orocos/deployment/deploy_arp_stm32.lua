8b9c189ad9b0fa6403eb5fa13b36003cb9ad88b9
willy <willy@c9b90f90-cbc1-11df-9d65-af01d29d43da>
2014-04-24 20:07:04 +0000

    git-svn-id: svn://team-ard.com/ARP/trunk@1836 c9b90f90-cbc1-11df-9d65-af01d29d43da

--- arp_stm32/script/orocos/deployment/deploy_arp_stm32.lua
+++ arp_stm32/script/orocos/deployment/deploy_arp_stm32.lua
@@ -8,13 +8,14 @@ print("début déploiment arp_stm32")
 -- chargement des librairies contenants les composants d'arp_stm32
 Deployer:import("arp_stm32");
 
+dofile("/opt/ard/arp_stm32/script/orocos/deployment/components/discovery_monitor_deployer.lua");
 dofile("/opt/ard/arp_stm32/script/orocos/deployment/components/discovery.lua");
-dofile("/opt/ard/arp_stm32/script/orocos/deployment/components/match_data.lua");
 dofile("/opt/ard/arp_stm32/script/orocos/deployment/components/dynamixels.lua");
-dofile("/opt/ard/arp_stm32/script/orocos/deployment/components/suction_pumps.lua");
-dofile("/opt/ard/arp_stm32/script/orocos/deployment/components/tor.lua");
 dofile("/opt/ard/arp_stm32/script/orocos/deployment/components/gyrometer.lua");
 dofile("/opt/ard/arp_stm32/script/orocos/deployment/components/hmi.lua");
+dofile("/opt/ard/arp_stm32/script/orocos/deployment/components/match_data.lua");
+dofile("/opt/ard/arp_stm32/script/orocos/deployment/components/suction_pumps.lua");
+dofile("/opt/ard/arp_stm32/script/orocos/deployment/components/tor.lua");
 
 print("... load components")
 assert( DiscoveryDeployer:load(), 		"Failed to load Discovery")
@@ -23,6 +24,7 @@ assert( DynamixelsDeployer:load(), 		"Failed to load Dynamixels")
 assert( SuctionPumpsDeployer:load(), 	"Failed to load SuctionPumps")
 assert( TorDeployer:load(), 			"Failed to load Tor")
 assert( GyrometerDeployer:load(), 		"Failed to load Gyrometer")
+assert( DiscoveryMonitorDeployer:load(),"Failed to load DiscoveryMonitor")
 
 print("... connect components")
 assert( DiscoveryDeployer:connect(), 	"Failed to connect Discovery")
@@ -31,6 +33,7 @@ assert( DynamixelsDeployer:connect(), 	"Failed to connect Dynamixels")
 assert( SuctionPumpsDeployer:connect(), "Failed to connect SuctionPumps")
 assert( TorDeployer:connect(), 			"Failed to connect Tor")
 assert( GyrometerDeployer:connect(),	"Failed to connect Gyrometer")
+assert( DiscoveryMonitorDeployer:connect(),"Failed to connect DiscoveryMonitor")
 
 print("... start components")
 assert( DiscoveryDeployer:start(), 		"Failed to start Discovery")
@@ -39,6 +42,7 @@ assert( DynamixelsDeployer:start(), 	"Failed to start Dynamixels")
 assert( SuctionPumpsDeployer:start(), 	"Failed to start SuctionPumps")
 assert( TorDeployer:start(), 			"Failed to start Tor")
 assert( GyrometerDeployer:start(), 		"Failed to start Gyrometer")
+assert( DiscoveryMonitorDeployer:start(),"Failed to start DiscoveryMonitor")
 
 print("fin déploiment arp_stm32")
 print("====================")
--- arp_stm32/script/orocos/deployment/deploy_arp_stm32_simul.lua
+++ arp_stm32/script/orocos/deployment/deploy_arp_stm32_simul.lua
@@ -8,13 +8,15 @@ print("début déploiment arp_stm32")
 -- chargement des librairies contenants les composants d'arp_stm32
 Deployer:import("arp_stm32");
 
+dofile("/opt/ard/arp_stm32/script/orocos/deployment/components/discovery_monitor_deployer.lua");
 dofile("/opt/ard/arp_stm32/script/orocos/deployment/components/simulated_discovery.lua");
-dofile("/opt/ard/arp_stm32/script/orocos/deployment/components/match_data.lua");
 dofile("/opt/ard/arp_stm32/script/orocos/deployment/components/dynamixels.lua");
-dofile("/opt/ard/arp_stm32/script/orocos/deployment/components/suction_pumps.lua");
-dofile("/opt/ard/arp_stm32/script/orocos/deployment/components/tor.lua");
 dofile("/opt/ard/arp_stm32/script/orocos/deployment/components/gyrometer.lua");
 dofile("/opt/ard/arp_stm32/script/orocos/deployment/components/hmi.lua");
+dofile("/opt/ard/arp_stm32/script/orocos/deployment/components/match_data.lua");
+dofile("/opt/ard/arp_stm32/script/orocos/deployment/components/suction_pumps.lua");
+dofile("/opt/ard/arp_stm32/script/orocos/deployment/components/tor.lua");
+
 
 print("... load components")
 assert( SimulatedDiscoveryDeployer:load(), 	"Failed to load SimulatedDiscovery")
@@ -24,6 +26,7 @@ assert( SuctionPumpsDeployer:load(), 		"Failed to load SuctionPumps")
 assert( TorDeployer:load(), 				"Failed to load Tor")
 assert( GyrometerDeployer:load(), 			"Failed to load Gyrometer")
 assert( HmiDeployer:load(), 		    	"Failed to load Hmi")
+assert( DiscoveryMonitorDeployer:load(),	"Failed to load DiscoveryMonitor")
 
 print("... connect components")
 assert( SimulatedDiscoveryDeployer:connect(),"Failed to connect SimulatedDiscovery")
@@ -33,9 +36,11 @@ assert( SuctionPumpsDeployer:connect(),	 	"Failed to connect SuctionPumps")
 assert( TorDeployer:connect(), 				"Failed to connect Tor")
 assert( GyrometerDeployer:connect(),		"Failed to connect Gyrometer")
 assert( HmiDeployer:connect(), 	      	 	"Failed to connect Hmi")
+assert( DiscoveryMonitorDeployer:connect(),	"Failed to connect DiscoveryMonitor")
 
 print("... start components")
 assert( SimulatedDiscoveryDeployer:start(), "Failed to start SimulatedDiscovery")
+--TODO add a way to drive the qemu simulation
 --assert( MatchDataDeployer:start(), 			"Failed to start MatchData")
 assert( DynamixelsDeployer:start(), 		"Failed to start Dynamixels")
 assert( SuctionPumpsDeployer:start(), 		"Failed to start SuctionPumps")
@@ -43,6 +48,7 @@ assert( TorDeployer:start(), 				"Failed to start Tor")
 assert( GyrometerDeployer:start(), 			"Failed to start Gyrometer")
 --TODO bug opengl
 --assert( HmiDeployer:start(), 		        "Failed to start Hmi")
+assert( DiscoveryMonitorDeployer:start(),"Failed to start DiscoveryMonitor")
 
 print("fin déploiment arp_stm32")
 print("====================")
