5dc9140d016f3e6c5c3d239a4def2655eb9d0709
willy <willy@c9b90f90-cbc1-11df-9d65-af01d29d43da>
2014-05-05 22:50:41 +0000

    scheduling ok
    git-svn-id: svn://team-ard.com/ARP/trunk@1874 c9b90f90-cbc1-11df-9d65-af01d29d43da

---
+++ ubiquity/src/ubiquity_robot/a trier/deploy_arp_stm32.lua
@@ -0,0 +1,43 @@
+require("rttlib")
+
+rttlib.color=true
+Deployer = rtt.getTC()
+print("====================")
+print("début déploiment arp_stm32")
+
+-- chargement des librairies contenants les composants d'arp_stm32
+Deployer:import("arp_stm32");
+
+dofile("/opt/ard/arp_stm32/script/orocos/deployment/components/discovery_monitor_deployer.lua");
+dofile("/opt/ard/arp_stm32/script/orocos/deployment/components/discovery.lua");
+dofile("/opt/ard/arp_stm32/script/orocos/deployment/components/dynamixels.lua");
+dofile("/opt/ard/arp_stm32/script/orocos/deployment/components/gyrometer.lua");
+dofile("/opt/ard/arp_stm32/script/orocos/deployment/components/hmi.lua");
+dofile("/opt/ard/arp_stm32/script/orocos/deployment/components/match_data.lua");
+dofile("/opt/ard/arp_stm32/script/orocos/deployment/components/suction_pumps.lua");
+dofile("/opt/ard/arp_stm32/script/orocos/deployment/components/tor.lua");
+
+print("... load components")
+assert( DiscoveryDeployer:load(), 		"Failed to load Discovery")
+assert( MatchDataDeployer:load(), 		"Failed to load MatchData")
+assert( DynamixelsDeployer:load(), 		"Failed to load Dynamixels")
+assert( SuctionPumpsDeployer:load(), 	"Failed to load SuctionPumps")
+assert( TorDeployer:load(), 			"Failed to load Tor")
+assert( GyrometerDeployer:load(), 		"Failed to load Gyrometer")
+assert( DiscoveryMonitorDeployer:load(),"Failed to load DiscoveryMonitor")
+
+print("... connect components")
+assert( DiscoveryDeployer:connect(), 	"Failed to connect Discovery")
+assert( MatchDataDeployer:connect(), 	"Failed to connect MatchData")
+assert( DynamixelsDeployer:connect(), 	"Failed to connect Dynamixels")
+assert( SuctionPumpsDeployer:connect(), "Failed to connect SuctionPumps")
+assert( TorDeployer:connect(), 			"Failed to connect Tor")
+assert( GyrometerDeployer:connect(),	"Failed to connect Gyrometer")
+assert( DiscoveryMonitorDeployer:connect(),"Failed to connect DiscoveryMonitor")
+
+print("... start components")
+assert( DiscoveryMonitorDeployer:start(),"Failed to start DiscoveryMonitor")
+assert( DiscoveryDeployer:start(), 		"Failed to start Discovery")
+
+print("fin déploiment arp_stm32")
+print("====================")
