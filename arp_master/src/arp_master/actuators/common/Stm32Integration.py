141298bcd59bed26c2ec016aa77aa9be896203c6
willy <willy@c9b90f90-cbc1-11df-9d65-af01d29d43da>
2014-05-12 21:11:53 +0000

    Add Omron integration
    git-svn-id: svn://team-ard.com/ARP/trunk@1895 c9b90f90-cbc1-11df-9d65-af01d29d43da

--- arp_master/src/arp_master/actuators/common/Stm32Integration.py
+++ arp_master/src/arp_master/actuators/common/Stm32Integration.py
@@ -18,11 +18,12 @@ from std_msgs.msg import *
 #     _ ArmPump
 # @param suctionPower : drive the suction power in percent in the range [50;100]
 class SendPumpCmd(SendOnTopic):
-    def __init__(self, pumpName, suctionPower):
-        SendOnTopic.__init__(self, "/Ubiquity/"+pumpName+"/suction_power", UInt8, UInt8(suctionPower))
+    def __init__(self, p_pumpName, p_suctionPower):
+        SendOnTopic.__init__(self, "/Ubiquity/"+p_pumpName+"/suction_power", UInt8, UInt8(p_suctionPower))
 
 #
 # You may drive a dynamixel with this state. It send a single command, it is *not* periodic.
+# Basically it means that you are *NOT* at the desired position when you exit this state
 # @param dynamixelName : the name of the dynamixel to drive :
 #     _ LeftFinger
 #     _ RightFinger
@@ -31,6 +32,42 @@ class SendPumpCmd(SendOnTopic):
 #     _ LeftCannonStocker
 #     _ RightCannonStocker
 # @param position : position cmd in [-pi;pi] in radians
-class DynamixelGotoPosition(SendOnTopic):
-    def __init__(self, dynamixelName, position):
-        SendOnTopic.__init__(self, "/Ubiquity/"+dynamixelName+"/position_cmd", Float32, Float32(position))
+class DynamixelNonBlockingPositionCmd(SendOnTopic):
+    def __init__(self, p_dynamixelName, p_position):
+        SendOnTopic.__init__(self, "/Ubiquity/"+p_dynamixelName+"/position_cmd", Float32, Float32(p_position))
+      
+#
+# Use this state to wait an Omron to be in an expected state
+# @param p_omronName : the omron to listen
+# @param p_awaitedValue : 'True' or 'False' The value you are waiting until the timeout.
+class WaitForOmronValue(CyclicState):
+    def __init__(self, p_omronName, p_awaitedValue, p_timeout):
+        CyclicState.__init__(self, outcomes=['triggered'], timeout=p_timeout) 
+        #remember topic
+        self.topicName = "/Ubiquity/"+p_omronName+"/object_present"
+        
+        #remember reference value
+        self.awaitedValue = p_awaitedValue
+        
+        #Keep an handle on the subscribe, else it would be destructed
+        self.subscriber = None
+        
+        #Buffer updated by the callback to be used when testing transitions
+        self.objectPresent = False
+    
+    def callback(self,msg):
+        self.objectPresent = msg.data
+        
+    #The topic is only listened when entering the state to prevent useless trigger when not active       
+    def executeIn(self):
+        self.subscriber = rospy.Subscriber(self.topicName, Bool, self.callback)
+        
+    #Leaving the handle will destruct the subscriber
+    def executeOut(self):
+        self.subscriber.unregister()
+        self.objectPresent = False
+        self.subscriber = None
+        
+    def executeTransitions(self):
+       if self.objectPresent == self.awaitedValue:
+            return 'triggered'  
