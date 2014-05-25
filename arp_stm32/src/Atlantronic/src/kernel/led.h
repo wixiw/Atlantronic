026d38d58ac40ceea71a109725909bc6d4da279f
Willy Lambert <lambert.willy@gmail.com>
2014-05-25 12:09:49 +0200

    Preparation mise en sous module Atlantronic

--- arp_stm32/src/Atlantronic/src/kernel/led.h
+++
@@ -1,6 +0,0 @@
-#ifndef LED_H
-#define LED_H
-
-void setLed(uint32_t mask);
-
-#endif
--- arp_stm32/src/Atlantronic/src/linux/test/gen_circle/build.mk
+++
@@ -1,3 +0,0 @@
-obj-linux-gen_circle += linux/test/gen_circle/gen_circle.o
-lib-linux-gen_circle += -lm
-bin-linux += gen_circle
