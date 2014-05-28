5f63900f0c107e03e8064dc0a26dfa8ced33af16
Tredez Jean-Baptiste <jbtredez@gmail.com>
2014-05-29 01:23:23 +0200

    HokuyoItf

---
+++ arp_stm32/src/components/sensors/HokuyoItf.hpp
@@ -0,0 +1,38 @@
+#ifndef HOKUYO_ITF_HPP_
+#define HOKUYO_ITF_HPP_
+
+#include "components/taskcontexts/Stm32TaskContext.hpp"
+#include "linux/tools/robot_interface.h"
+#include "ros/ros.h"
+
+#include <std_msgs/Bool.h>
+
+namespace arp_stm32
+{
+
+class HokuyoItf: public Stm32TaskContext
+{
+    public:
+        HokuyoItf(const std::string& name);
+
+        /****************************************************************
+         * Interface Orocos
+         ****************************************************************/
+
+        bool configureHook();
+        void updateHook();
+
+        RTT::OutputPort<hokuyo_scan> outScan;
+
+        /****************************************************************
+         * Interface ROS
+         ****************************************************************/
+
+    protected:
+        void createOrocosInterface();
+
+        RobotInterface& m_robotItf;
+};
+
+} /* namespace arp_stm32 */
+#endif /* HOKUYO_ITF_HPP_ */
