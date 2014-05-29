bf8c8998fd0ec5459e4b74f2489e2cefe1e5899b
Willy Lambert <lambert.willy@gmail.com>
2014-05-29 06:15:34 +0200

    Recuperation de la simu de big_refactoring

---
+++ arp_stm32/src/simulation/SimulatedDiscovery.hpp
@@ -0,0 +1,43 @@
+/*
+ * SimulatedDiscovery.hpp
+ *
+ *  Created on: 22 April 2014
+ *      Author: wla
+ */
+
+#ifndef SIMULATEDDISCOVERY_HPP_
+#define SIMULATEDDISCOVERY_HPP_
+
+#include "components/discovery/Discovery.hpp"
+#include "linux/tools/qemu.h"
+#include <std_msgs/Bool.h>
+
+namespace arp_simu
+{
+
+class SimulatedDiscovery: public arp_stm32::Discovery
+{
+    public:
+        SimulatedDiscovery(const std::string& name);
+        ~SimulatedDiscovery();
+
+        bool configureHook();
+        void updateHook();
+        bool breakUpdateHook();
+
+    protected:
+        void createOrocosInterface();
+
+        Qemu m_qemu;
+        std::string propStm32ExecutableName;
+
+        //Ports used to drive qemu IOs
+        RTT::InputPort<std_msgs::Bool> inFakeUserButton1;
+        RTT::InputPort<std_msgs::Bool> inFakeUserButton2;
+        RTT::InputPort<std_msgs::Bool> inFakeStart;
+
+        bool attrFakeStart;
+};
+
+} /* namespace arp_stm32 */
+#endif /* SIMULATEDDISCOVERY_HPP_ */
