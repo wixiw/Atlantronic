816a7a44eae7b4a50a54a21554f2537cb4892db0
Willy Lambert <lambert.willy@gmail.com>
2014-05-31 03:58:51 +0200

    Tout dépété
    Conflicts:
    
    	arp_stm32/src/Atlantronic

--- arp_master/src/arp_master/strat_2014/localStrategicActions_2014/FrescosStates.py
+++ arp_master/src/arp_master/strat_2014/localStrategicActions_2014/FrescosStates.py
@@ -28,7 +28,7 @@ class StickFrescosState(LocalStrategicAction):
         with self:          
             # Approach Fresco
             LocalStrategicAction.add('GoToFresco',
-                      AmbiOmniDirectOrder2(Pose2D(0.000, 1.000 - Robot2014.LEFT_SIDE.y - 0.050, pi+0.428)),
+                      AmbiOmniDirectOrder2(Pose2D(0.000, 1.000 - Robot2014.LEFT_SIDE.y - 0.150, -pi+0.428), vmax=Robot2014.motionSpeeds['Average']),
                       transitions={'succeeded':'StickFrescos', 'timeout':'failed'})
             self.setInitialState('GoToFresco')
             
@@ -36,7 +36,7 @@ class StickFrescosState(LocalStrategicAction):
                         
             # Stick Fresco
             LocalStrategicAction.add('StickFrescos',
-                      AmbiOmniDirectOrder2(Pose2D(0.000, 1.000 - 0.214 + 0.200, pi+0.428),vmax=0.3),
+                      AmbiOmniDirectOrder2(Pose2D(0.000, 1.000 - 0.214 + 0.200, -pi+0.428), vmax=Robot2014.motionSpeeds['Carefull']),
                       transitions={#a motion success mean that we didn't bumped the wall so it's a problem
                                    'succeeded':'failed', 
                                    #a motion failure mean that we bumped the wall, so it's a success
@@ -47,4 +47,37 @@ class StickFrescosState(LocalStrategicAction):
                       AmbiOmniDirectOrder2(self.getEntryYellowPose()),
                       transitions={'succeeded':'succeeded', 'timeout':'failed'})
             
-        
+class ReverseStickFrescosState(LocalStrategicAction):
+
+    @staticmethod
+    def getEntryYellowPoseStatic():
+        return Pose2D(0.000, 0.400, -0.428);
+    
+    def getEntryYellowPose(self):
+        return self.getEntryYellowPoseStatic();
+    
+    def __init__(self):
+        LocalStrategicAction.__init__(self, Robot2014.SWITCH_TO_EOG_DELAY)
+        with self:          
+            # Approach Fresco
+            LocalStrategicAction.add('GoToFresco',
+                      AmbiOmniDirectOrder2(Pose2D(0.000, 1.000 - Robot2014.LEFT_SIDE.y - 0.150, -0.428), vmax=Robot2014.motionSpeeds['Average']),
+                      transitions={'succeeded':'ReverseStickFrescos', 'timeout':'failed'})
+            self.setInitialState('GoToFresco')
+            
+            #Utiliser un ordre d'approche pour gerer la collision avec le mur (peut venir du timeout precedent)
+                        
+            # Stick Fresco
+            LocalStrategicAction.add('ReverseStickFrescos',
+                      AmbiOmniDirectOrder2(Pose2D(0.000, 1.000 - 0.214 + 0.200, -0.428),vmax=Robot2014.motionSpeeds['Carefull']),
+                      transitions={#a motion success mean that we didn't bumped the wall so it's a problem
+                                   'succeeded':'failed', 
+                                   #a motion failure mean that we bumped the wall, so it's a success
+                                   'timeout':'ReverseQuitFresco'})
+
+            # Quit Fresco
+            LocalStrategicAction.add('ReverseQuitFresco',
+                      AmbiOmniDirectOrder2(self.getEntryYellowPose()),
+                      transitions={'succeeded':'succeeded', 'timeout':'failed'})
+            
+                
