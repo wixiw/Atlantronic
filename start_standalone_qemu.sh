ac951ba76bd294c10c78c7f862c71c5ce422dded
Willy Lambert <lambert.willy@gmail.com>
2014-09-25 01:10:04 +0200

    change binary for simulation

--- start_standalone_qemu.sh
+++ start_standalone_qemu.sh
@@ -6,7 +6,8 @@ mkfifo /tmp/carte-test.in
 mkfifo /tmp/carte-test.out
 
 VERSION_GIT=96739a5bcc6e5b608c18fa1ac371e34dbe7e2478
+STM32_BIN_PATH=/opt/ard/arp_stm32/src/Atlantronic/bin/discovery/baz_small
 
 echo "Launching qemu..."
-./qemu/arm-softmmu/qemu-system-arm -M atlantronic -nodefaults -nographic -chardev pipe,id=foo_usb,path=/tmp/carte-test -chardev pipe,id=foo_model,path=/tmp/qemu-test -kernel /opt/ard/arp_stm32/bin_$VERSION_GIT/discovery/baz_small
+./qemu/arm-softmmu/qemu-system-arm -M atlantronic -nodefaults -nographic -chardev pipe,id=foo_usb,path=/tmp/carte-test -chardev pipe,id=foo_model,path=/tmp/qemu-test -kernel $STM32_BIN_PATH
 echo "end."
