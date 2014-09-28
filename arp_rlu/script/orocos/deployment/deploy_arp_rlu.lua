103e96418ba13e79cc214d13e0edd8ef2044f950
Willy Lambert <lambert.willy@gmail.com>
2014-09-28 02:12:23 +0200

    Remise en route du robot post debug JB

--- arp_rlu/script/orocos/deployment/deploy_arp_rlu.lua
+++ arp_rlu/script/orocos/deployment/deploy_arp_rlu.lua
@@ -3,7 +3,6 @@ rttlib.color=true
 Deployer = rtt.getTC()
 print("====================")
 print("début déploiment arp_rlu")
-
 -- chargement des librairies contenants les composants d'arp_ods
 Deployer:import("arp_rlu");
 Deployer:import("rtt_sensor_msgs");
@@ -17,15 +16,12 @@ dofile("/opt/ard/arp_rlu/script/orocos/deployment/components/ros_rlu_itf_deploye
 
 OdometryDeployer:load();
 LocalizatorDeployer:load();
---ObstacleDetector:load();
 ObstacleManager:load();
 RosRluItfDeployer:load();
 RluMonitorDeployer:load();
 
-
 OdometryDeployer:connect();
 LocalizatorDeployer:connect();
---ObstacleDetector:connect();
 ObstacleManager:connect();
 RosRluItfDeployer:connect();
 RluMonitorDeployer:connect();
--- screen.sh
+++ screen.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-screen -a -D -m -h 2000 -S strat_ard bash -c /opt/ard/boot.sh&
+#screen -a -D -m -h 2000 -S strat_ard bash -c /opt/ard/boot.sh&
 
